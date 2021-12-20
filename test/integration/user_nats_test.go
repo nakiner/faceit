0f8c7952372dfcfd36756ccf85bf3263cc90b2dc
Pavel Emelyanov <p.emelyanov@agimagroup.ru>
2021-12-20 04:56:00 +0300

    add nats subscriber test

--- test/integration/user_nats_test.go
+++ test/integration/user_nats_test.go
@@ -4,65 +4,92 @@
 package integration
 
 import (
+	"fmt"
+	"github.com/nakiner/faceit/configs"
 	"github.com/nakiner/faceit/pkg/queue/user"
-	"github.com/stretchr/testify/assert"
 	"log"
-	"sync"
 	"testing"
+	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNatsPublisherUserServiceUpdateUser(t *testing.T) {
-	nc, err := nats.Connect(nats.DefaultURL)
+	cfg := configs.NewConfig()
+	err := cfg.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nc, err := nats.Connect(
+		fmt.Sprintf("nats://%s:%d", cfg.Nats.Host, cfg.Nats.Port),
+		nats.ReconnectWait(time.Millisecond*time.Duration(cfg.Nats.WaitLimit)),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer nc.Close()
+
 	ec, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer ec.Close()
+
 	p, err := user.NewPublisher(ec)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = p.UpdateUser(&user.User{})
+	err = p.UpdateUser(&user.User{Nickname: "sample"})
 	require.NoError(t, err)
 }
 
 func TestNatsSubscriberUserServiceUpdateUser(t *testing.T) {
-	nc, err := nats.Connect(nats.DefaultURL)
+	cfg := configs.NewConfig()
+	err := cfg.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nc, err := nats.Connect(
+		fmt.Sprintf("nats://%s:%d", cfg.Nats.Host, cfg.Nats.Port),
+		nats.ReconnectWait(time.Millisecond*time.Duration(cfg.Nats.WaitLimit)),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer nc.Close()
+
+	ch := make(chan string)
+	s := user.NewSubscriber(nc)
+	err = s.UpdateUser(func(u *user.User) {
+		ch <- u.ID
+	})
+	require.NoError(t, err)
+
 	ec, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer ec.Close()
+
+	us := user.User{
+		ID: "sample",
+	}
+
 	p, err := user.NewPublisher(ec)
-	if err != nil {
-		log.Fatal(err)
+	assert.NoError(t, err)
+
+	for i := 0; i < 5; i++ {
+		err = p.UpdateUser(&us)
+		assert.NoError(t, err)
 	}
-	n := "sample"
-	err = p.UpdateUser(&user.User{Nickname: "smpl"})
-	if err != nil {
-		log.Fatal(err)
-	}
-	require.NoError(t, err)
 
-	var wg sync.WaitGroup
+	time.Sleep(time.Second * 1)
 
-	wg.Add(1)
+	res := <-ch
 
-	s := user.NewSubscriber(nc)
-	err = s.UpdateUser(func(u *user.User) {
-		assert.Equal(t, n, u.Nickname)
-	})
-
-	wg.Wait()
-
-	require.NoError(t, err)
+	assert.Equal(t, us.ID, res)
 }
